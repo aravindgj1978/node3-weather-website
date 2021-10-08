98c27076cace8a847f818a3d01fccc6784a0dffc
aravindgj1978 <aravindgj@gmail.com>
2021-10-08 14:11:36 +0530

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=bea2022cb81dd268c0bf379266435308&query=' + latitude + ',' + longitude
+
+    request({url, json:true}, (error, {body}) => {
+        if(error) {
+            callback('Unable to connect to weather service', undefined)
+        }
+        else if(body.error) {
+            callback(body.error.info, undefined)
+        } else {
+            callback(undefined, {
+                weatherDescription: body.current.weather_descriptions[0],
+                currentTemperature: body.current.temperature,
+                feelsLike: body.current.feelslike
+            })
+        }
+    })
+}
+
+module.exports = forecast
